18be942f393e8d35fcd24663f4e14eab371a8d5d
Андрей <90196953+w1nway@users.noreply.github.com>
2023-10-06 00:35:42 +0300

    Create timeserver.py

---
+++ hw1/timeserver.py
@@ -0,0 +1,20 @@
+import socket
+from datetime import datetime
+
+def main():
+    sock = socket.socket()
+    sock.bind(("0.0.0.0", 1303))
+    sock.listen(1)
+
+    while True:
+        conn, addr = sock.accept()
+        current_datetime = datetime.now().strftime("%d.%m.%Y %H:%M")
+        conn.send(current_datetime.encode("UTF-8"))
+        conn.close()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        exit
